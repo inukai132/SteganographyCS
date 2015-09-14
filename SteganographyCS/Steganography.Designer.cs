13ffb4fb08ea17a20b9bc261a2913358391b5d6d
Tylor Childers <tylord1@gmail.com>
2015-09-14 11:32:02 -0400

    UI Work

---
+++ SteganographyCS/Steganography.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SteganographyCS
+{
+    partial class Steganography
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browseTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.browseButton = new System.Windows.Forms.Button();
+            this.stegTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.editButton = new System.Windows.Forms.Button();
+            this.okButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.loadButton = new System.Windows.Forms.Button();
+            this.fileLoadDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // browseTextBox
+            // 
+            this.browseTextBox.Location = new System.Drawing.Point(12, 33);
+            this.browseTextBox.Name = "browseTextBox";
+            this.browseTextBox.Size = new System.Drawing.Size(519, 20);
+            this.browseTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Choose the image";
+            // 
+            // browseButton
+            // 
+            this.browseButton.Location = new System.Drawing.Point(94, 59);
+            this.browseButton.Name = "browseButton";
+            this.browseButton.Size = new System.Drawing.Size(75, 23);
+            this.browseButton.TabIndex = 2;
+            this.browseButton.Text = "Browse";
+            this.browseButton.UseVisualStyleBackColor = true;
+            this.browseButton.Click += new System.EventHandler(this.browseButton_Click);
+            // 
+            // stegTextBox
+            // 
+            this.stegTextBox.Location = new System.Drawing.Point(12, 119);
+            this.stegTextBox.Multiline = true;
+            this.stegTextBox.Name = "stegTextBox";
+            this.stegTextBox.ReadOnly = true;
+            this.stegTextBox.Size = new System.Drawing.Size(519, 261);
+            this.stegTextBox.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "This is the steg data";
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(16, 388);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(75, 23);
+            this.editButton.TabIndex = 5;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            // 
+            // okButton
+            // 
+            this.okButton.Location = new System.Drawing.Point(98, 388);
+            this.okButton.Name = "okButton";
+            this.okButton.Size = new System.Drawing.Size(75, 23);
+            this.okButton.TabIndex = 6;
+            this.okButton.Text = "Ok";
+            this.okButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(180, 388);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 7;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // loadButton
+            // 
+            this.loadButton.Location = new System.Drawing.Point(12, 59);
+            this.loadButton.Name = "loadButton";
+            this.loadButton.Size = new System.Drawing.Size(75, 23);
+            this.loadButton.TabIndex = 8;
+            this.loadButton.Text = "Load";
+            this.loadButton.UseVisualStyleBackColor = true;
+            this.loadButton.Click += new System.EventHandler(this.loadButton_Click);
+            // 
+            // fileLoadDialog
+            // 
+            this.fileLoadDialog.FileName = "fileLoadDialog";
+            this.fileLoadDialog.Filter = "JPG|*.jpg;*.gif;*.bmp";
+            this.fileLoadDialog.RestoreDirectory = true;
+            this.fileLoadDialog.SupportMultiDottedExtensions = true;
+            this.fileLoadDialog.Title = "Load Image";
+            this.fileLoadDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.fileLoadDialog_FileOk);
+            // 
+            // Steganography
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 423);
+            this.Controls.Add(this.loadButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.okButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.stegTextBox);
+            this.Controls.Add(this.browseButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.browseTextBox);
+            this.Name = "Steganography";
+            this.Text = "Steganography";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox browseTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button browseButton;
+        private System.Windows.Forms.TextBox stegTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button okButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button loadButton;
+        private System.Windows.Forms.OpenFileDialog fileLoadDialog;
+    }
+}
+
---
+++ SteganographyCS/Steganography.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteganographyCS
+{
+    public partial class Steganography : Form
+    {
+        FileStream image;
+
+        public Steganography()
+        {
+            InitializeComponent();
+        }
+
+        private void loadButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void browseButton_Click(object sender, EventArgs e)
+        {
+            fileLoadDialog.ShowDialog();
+        }
+
+        private void fileLoadDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            browseTextBox.Text = fileLoadDialog.FileName;
+            image = (FileStream)fileLoadDialog.OpenFile();
+        }
+
+        private void Steganography_Resize(object sender, System.EventArgs e)
+        {
+            browseTextBox.Width = this.Width - 41;
+            stegTextBox.Width = browseTextBox.Width;
+        }
+    }
+}
